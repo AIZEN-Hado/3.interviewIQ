4e7f46d82edde5d7c954d31713936e8b61191039
Voldemort19 <yashsinghashoksinghchauhan2022@vitbhopal.ac.in>
2026-05-04 14:05:34 +0530

    mongo update

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "862159592601",
-  appId: "1:862159592601:web:7308d702cd708076ddec08"
+  authDomain: "iqagent-f3684.firebaseapp.com",
+  projectId: "iqagent-f3684",
+  storageBucket: "iqagent-f3684.firebasestorage.app",
+  messagingSenderId: "329546030035",
+  appId: "1:329546030035:web:0b669793147dfdc42aad26"
 };
 
 const app = initializeApp(firebaseConfig);
